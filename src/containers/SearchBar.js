522a529532041c101ad0b43cbab3eaaf931af2c4
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-04-25 15:50:27 -0700

    Implemented search bar features weather api

--- src/containers/SearchBar.js
+++ src/containers/SearchBar.js
@@ -1,18 +1,52 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault();
+
+    //Call action creator. We are using this.props.fetchWeather beause of fetchWeather was passed in in mapDispatchToProps.
+    this.props.fetchWeather(this.state.term);
+
+    //Set the input field in the view back a blank space after submitting.
+    this.setState({ term: '' });
+  }
+
+  onInputChange(event) {
+    this.setState({ term: event.target.value });
+  }
+
   render() {
     return (
-      <div>
-        <form className="input-group">
-          <input
-            placeholder="Enter a city for a 5-day forecast"
-            className="form-control" />
-          <span className="input-group-btn">
-            <button className="btn btn-primary">Search</button>
-          </span>
-        </form>
-      </div>
+      <form onSubmit={this.onFormSubmit} className="input-group">
+        <input
+          placeholder="Enter a city for a 5-day forecast"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange} />
+        <span className="input-group-btn">
+          <button type="submit" className="btn btn-secondary">Search</button>
+        </span>
+      </form>
     );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  //Makes sure the action flows through the middleware and into the reducers
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+//mapDispatchToProps needs to be the second argument. So, we use null to say that no state is being used.
+export default connect(null, mapDispatchToProps)(SearchBar);

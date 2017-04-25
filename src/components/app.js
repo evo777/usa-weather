f3be43a5d3d91366526609dfd97fa95dcb2046b2
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-04-24 20:52:38 -0700

    Render SearchBar component

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 
+import SearchBar from '../containers/SearchBar';
+
 export default class App extends Component {
   render() {
     return (
-      <div>Hello World</div>
+      <div>
+        <SearchBar />
+      </div>
     );
   }
 }

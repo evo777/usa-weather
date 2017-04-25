5ce2a3c8590abc284c9f68fc389d9adefb46a6ea
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-04-25 15:49:48 -0700

    Added reducerWeather to a single reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
+import reducerWeather from './reducerWeather';
+
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  weather: reducerWeather
 });
 
 export default rootReducer;

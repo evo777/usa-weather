a09a6ccb5c95a36388c9641a0400e8987da8ea75
Emmanuel Vo <emmanuelvo777@gmail.com>
2017-04-25 18:09:52 -0700

    Added more styling

--- src/containers/Weather.js
+++ src/containers/Weather.js
@@ -25,19 +25,21 @@ class Weather extends Component {
 
   render() {
     return (
-      <table className="table table-hover">
-        <thead>
-          <tr>
-            <th>City</th>
-            <th>Temperature (C)</th>
-            <th>Pressure (hPa)</th>
-            <th>Humidity (%)</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.props.weather.map(this.renderWeather)}
-        </tbody>
-      </table>
+      <div className="table-screen">
+        <table className="table table-hover">
+          <thead>
+            <tr>
+              <th>City</th>
+              <th>Temperature (C)</th>
+              <th>Pressure (hPa)</th>
+              <th>Humidity (%)</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.weather.map(this.renderWeather)}
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
